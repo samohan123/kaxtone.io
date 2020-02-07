a6e23cfff120faa794ae3a8cf59aea944a2e9866
SaM <admin@kaxtone.com>
2020-02-07 16:29:20 +0530

    Updates

---
+++ precache-manifest.5015f758bfb62181faf9614a6ceb3907.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b8f976cbdc242591d7cc546ca2724b",
+    "url": "/kaxtone_waitforit/index.html"
+  },
+  {
+    "revision": "d4864c2856c40af43745",
+    "url": "/kaxtone_waitforit/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "5a3dccf76dec1029e2e1",
+    "url": "/kaxtone_waitforit/static/js/2.e01216c0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kaxtone_waitforit/static/js/2.e01216c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4864c2856c40af43745",
+    "url": "/kaxtone_waitforit/static/js/main.53662319.chunk.js"
+  },
+  {
+    "revision": "b94eca25068904f78289",
+    "url": "/kaxtone_waitforit/static/js/runtime-main.cd1dddb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaxtone_waitforit/static/media/logo.5d5d9eef.svg"
+  }
+]);
